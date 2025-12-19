f7c0ee70e19c5845d708be588c8721dc76251b96
harinivassahana-max <harinivassahana@gmail.com>
2025-12-19 16:51:51 +1100

    activity 3

---
+++ activity 3.py
@@ -0,0 +1,30 @@
+def hotel_cost(nights):
+    return 140*nights
+
+def plane_ride_cost(city):
+    if "Charlotte"==city:
+        return 183
+    elif "Tampa" == city:
+        return 220
+    elif "pittsburgh" == city:
+        return 222
+    elif "los angles" == city:
+        return 475
+    
+def rental_car_cost(days):
+    if days>=7:
+        return 40*days -50
+    elif days>=3:
+        return 40*days - 20
+    else:
+        return 40*days
+
+def trip_cost(city, days, spending_money):
+    return rental_car_cost(days) + hotel_cost(days) + plane_ride_cost(city) + spending_money
+
+print("cost of car rental:",rental_car_cost(5))
+print("cost of plane ride:", plane_ride_cost("los angles"))
+print("cost of hotel room:", hotel_cost(7))
+print("total cost of the trip:", trip_cost("los angles",7,500))
+print(trip_cost("tampa",6,500))
+      
